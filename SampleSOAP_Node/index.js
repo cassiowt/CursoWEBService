0e70bf1a4a0a965a3ab04eb808c1271c2dbd2d72
Cassio A W Trindade <cassiowt@gmail.com>
2022-06-23 19:22:08 -0300

    gerado exemplo de cliente consumindo SOAP

---
+++ SampleSOAP_Node/index.js
@@ -0,0 +1,29 @@
+
+var soap = require('soap');
+/*var url = 'http://www.gcomputer.net/webservices/dilbert.asmx?wsdl';
+soap.createClient(url, function(err, client) {
+    client.DailyDilbert({ ADate: "2022-06-02" }, function(err, result) {
+        if(err) return console.log(err);
+        console.log(result.DailyDilbertResult);
+    });
+});*/
+var url = 'http://www.dneonline.com/calculator.asmx?wsdl';
+soap.createClient(url, function(err, client) {
+    client.Add({intA: 2, intB: 3}, function (err, result) {
+        if (err) return console.log(err);
+        console.log("SOMA: ", result.AddResult);
+    });
+   client.Divide({ intA: 2, intB:3}, function(err, result) {
+        if(err) return console.log(err);
+        console.log("DIVISÃO: ", result.DivideResult);
+    });
+   client.Multiply({ intA: 2, intB:3}, function(err, result) {
+        if(err) return console.log(err);
+        console.log("MULTIPLICAÇÃO: ", result.MultiplyResult);
+    });
+   client.Subtract({ intA: 2, intB:3}, function(err, result) {
+        if(err) return console.log(err);
+        console.log("SUBTRAÇÃO: ", result.SubtractResult);
+    });
+    console.log("conteudo SOAP", client.lastRequest)
+});
