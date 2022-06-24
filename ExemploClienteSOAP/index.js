624f361f7e5b108011b06f5db2511e7f616351d6
cassio.trindade <cassiowt@gmail.com>
2022-06-24 12:54:03 -0300

    include log to  describe in descritor Soap

--- ExemploClienteSOAP/index.js
+++ ExemploClienteSOAP/index.js
@@ -1,12 +1,12 @@
 
 var soap = require('soap');
-/*var url = 'http://www.gcomputer.net/webservices/dilbert.asmx?wsdl';
-soap.createClient(url, function(err, client) {
+var url1 = 'http://www.gcomputer.net/webservices/dilbert.asmx?wsdl';
+soap.createClient(url1, function(err, client) {
     client.DailyDilbert({ ADate: "2022-06-02" }, function(err, result) {
         if(err) return console.log(err);
-        console.log(result.DailyDilbertResult);
     });
-});*/
+    console.log("DESCRIBE -->  ", client.describe().Dilbert.DilbertSoap);
+});
 var url = 'http://www.dneonline.com/calculator.asmx?wsdl';
 soap.createClient(url, function(err, client) {
     client.Add({intA: 2, intB: 3}, function (err, result) {
@@ -26,4 +26,5 @@ soap.createClient(url, function(err, client) {
         console.log("SUBTRAÇÃO: ", result.SubtractResult);
     });
     console.log("conteudo SOAP", client.lastRequest)
+    console.log("DESCRIBE -->  ", client.describe().Calculator.CalculatorSoap);
 });
